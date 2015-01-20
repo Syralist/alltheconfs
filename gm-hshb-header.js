29323a7c0c32559cc5560c0c81c22d7d0caa1a80
Thomas Helmke <thomas.helmke@gmx.de>
2015-01-20 16:57:27 +0100

    Greasemonkey Script for fixing hackerspace-bremen.de

---
+++ gm-hshb-header.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        Header runterschieben
+// @namespace   Hackerspace
+// @include     https://www.hackerspace-bremen.de/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+$('#head-wrapper').css('top', '32px');
