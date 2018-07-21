fa8e32a831d3a351512d59f4873f5ff7faa117cf
Thomas Helmke <thomas.helmke@gmx.de>
2018-07-21 13:30:12 +0200

    installiert nur noch stow. Homepfad nicht mehr hart codiert.

--- stowconfig.py
+++ stowconfig.py
@@ -1,8 +1,8 @@
 import subprocess
 import os
 
-# install requirements
-install_req = "apt-get install -y stow python3-pip".split()
+# install stow
+install_req = "apt-get install -y stow".split()
 sudo = subprocess.Popen(["sudo", "-S"] + install_req, stdout=subprocess.PIPE)
 print(sudo.stdout.read())
 
@@ -11,6 +11,6 @@ path = os.path.join(os.getcwd(), "stow")
 os.chdir(path)
 stow_packages = "git vim zsh tex tmux".split()
 for package in stow_packages:
-    stow_cmd = f"stow -t /home/thomas {package}".split()
+    stow_cmd = f"stow -t {os.path.expanduser('~')} {package}".split()
     stow = subprocess.run(stow_cmd, stdout=subprocess.PIPE, shell=False)
     print(stow)
