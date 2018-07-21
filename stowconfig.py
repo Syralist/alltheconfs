91f096cf1968f079355101ae29b7ec056668e02b
Thomas Helmke <thomas.helmke@gmx.de>
2018-07-21 12:29:04 +0200

    angepasst an dploy

--- stowconfig.py
+++ stowconfig.py
@@ -1,13 +1,18 @@
 import subprocess
 
-install_stow = "apt-get install stow".split()
-
-sudo = subprocess.Popen(["sudo", "-S"] + install_stow, stdout=subprocess.PIPE)
-
+# install requirements
+install_req = "apt-get install -y stow python3-pip".split()
+sudo = subprocess.Popen(["sudo", "-S"] + install_req, stdout=subprocess.PIPE)
 print(sudo.stdout.read())
 
-stow_cmd = "stow -d stow -t ~ -v 3 git vim zsh tex tmux".split()
+# install dploy
+install_dploy = "pip3 install dploy".split()
+sudo = subprocess.Popen(["sudo", "-S"] + install_dploy, stdout=subprocess.PIPE)
+print(sudo.stdout.read())
 
-stow = subprocess.Popen(stow_cmd, stdout=subprocess.PIPE)
-
-print(stow.stdout.read())
+# invoke dploy
+stow_packages = "git vim zsh tex tmux".split()
+for package in stow_packages:
+    dploy_cmd = f"python3 -m dploy stow stow/{package} ~".split()
+    stow = subprocess.Popen(dploy_cmd, stdout=subprocess.PIPE, shell=True)
+    print(stow.stdout.read())
