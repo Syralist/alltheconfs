112525861b1b1ce51c49fdc5224c3ca3f681b79a
Thomas Helmke <thomas.helmke@gmx.de>
2018-07-22 12:41:03 +0200

    zshconfig version 1

--- stowconfig.py
+++ stowconfig.py
@@ -2,11 +2,13 @@ import subprocess
 import os
 
 # install stow
+print("stow installieren")
 install_req = "apt-get install -y stow".split()
 sudo = subprocess.Popen(["sudo", "-S"] + install_req, stdout=subprocess.PIPE)
 print(sudo.stdout.read())
 
 # invoke stow
+print("stow ausführen")
 path = os.path.join(os.getcwd(), "stow")
 os.chdir(path)
 stow_packages = "git vim zsh tex tmux".split()
---
+++ zshconfig.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+
+# install zsh
+print("zsh installieren")
+install_req = "apt-get install -y zsh".split()
+sudo = subprocess.Popen(["sudo", "-S"] + install_req, stdout=subprocess.PIPE)
+print(sudo.stdout.read())
+
+# change to zsh
+print("zsh als Standard setzen")
+chsh_req = "chsh -s /usr/bin/zsh".split()
+chsh = subprocess.Popen(chsh_req, stdout=subprocess.PIPE)
+print(sudo.stdout.read())
