cbe15cd837396edf9df93e46311aec9fc3cba4c3
Thomas Helmke <thomas.helmke@gmx.de>
2018-07-22 11:42:02 +0200

    xclip durch xsel ersetzt

--- gitconfig.py
+++ gitconfig.py
@@ -13,13 +13,18 @@ ssh_cmd = 'ssh-add ~/.ssh/id_rsa'.split()
 ssh_add = subprocess.Popen(ssh_cmd, stdout=subprocess.PIPE, shell=True)
 print(ssh_add.stdout.read())
 
+# install xsel
+install_req = "apt-get install -y xsel".split()
+sudo = subprocess.Popen(["sudo", "-S"] + install_req, stdout=subprocess.PIPE)
+print(sudo.stdout.read())
+
 # copy ssh-key
 print("SSH Key kopieren")
 cat_cmd = 'cat ~/.ssh./id_rsa.pub'.split()
 cat = subprocess.Popen(cat_cmd, stdout=subprocess.PIPE, shell=True)
-xclip_cmd = 'xclip'.split()
-xclip = subprocess.Popen(xclip_cmd, stdin=cat.stdout, stdout=subprocess.PIPE, shell=False)
-print(xclip.stdout.read())
+xsel_cmd = 'xsel -bi'.split()
+xsel = subprocess.Popen(xsel_cmd, stdin=cat.stdout, stdout=subprocess.PIPE, shell=False)
+print(xsel.stdout.read())
 print("SSH Key ist in der Zwischenablage. Auf dieser Seite einfügen: https://github.com/settings/ssh/new ")
 wait = input("Enter drücken wenn es weitergehen kann.")
 
