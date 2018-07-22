b383ecf641d7df70b99eca3658d0f2368096c8e3
Thomas Helmke <thomas.helmke@gmx.de>
2018-07-22 19:28:50 +0200

    vundle installation

--- installpackages.py
+++ installpackages.py
@@ -3,6 +3,13 @@ import os
 
 # install packages for coding
 print("pakete installieren")
-install_req = "apt-get install -y aptitude vim-nox build-essentials python3-pip code gimp inkscape".split()
+install_req = "apt-get install -y aptitude vim-nox build-essential python3-pip code gimp inkscape".split()
 sudo = subprocess.Popen(["sudo", "-S"] + install_req, stdout=subprocess.PIPE)
 print(sudo.stdout.read())
+
+# install vundle
+print("vundle installieren")
+install_req = f"git clone https://github.com/VundleVim/Vundle.vim.git {os.path.expanduser('~/.vim/bundle/Vundle.vim')}".split()
+cmd = subprocess.Popen(install_req, stdout=subprocess.PIPE)
+print(cmd.stdout.read())
+
